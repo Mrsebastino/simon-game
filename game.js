260718d3f5a36c8be3a2428d85bce10f44aaafcf
mrsebastino <mrsebastino@gmail.com>
2021-02-16 16:42:13 +0000

    added logic to select random color

--- game.js
+++ game.js
@@ -1,1 +1,8 @@
+let gamePattern = [];
+let buttonColors = ["red", "blue", "green", "yellow"];
 
+function nextSequence() {
+  let randomNumber = Math.floor(Math.random() * 4);
+  let randomChosenColour = buttonColors[randomNumber]
+  gamePattern.push(randomChosenColour);
+}
