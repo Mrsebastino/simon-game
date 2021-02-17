0fbbfef05e1a17d110178ae67e2ad127d051f069
mrsebastino <mrsebastino@gmail.com>
2021-02-17 15:24:17 +0000

    added flash to selected color

--- game.js
+++ game.js
@@ -1,8 +1,13 @@
-let gamePattern = [];
 let buttonColors = ["red", "blue", "green", "yellow"];
+let gamePattern = [];
 
 function nextSequence() {
   let randomNumber = Math.floor(Math.random() * 4);
   let randomChosenColour = buttonColors[randomNumber]
   gamePattern.push(randomChosenColour);
+
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  let audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  audio.play();
+
 }
