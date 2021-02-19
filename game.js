62f3d2772167488866a63fac77aa212ee2966853
mrsebastino <mrsebastino@gmail.com>
2021-02-19 15:26:59 +0000

    aded the stargame functionality and level counter

--- game.js
+++ game.js
@@ -2,22 +2,37 @@ let buttonColors = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
 let userClickedPattern = [];
 
+let started = false;
+let level = 0;
+
+$(document).keydown(function() {
+    if (!started) {
+      $("#level-title").text("Level " + level);
+      nextSequence();
+      started = true;
+    }
+});
+
 // What a BTN will do evetime we press it
 $(".btn").click(function() {
    let userChosenColor = $(this).attr("id");
    userClickedPattern.push(userChosenColor);
    playSound(userChosenColor);
    animatePress(userChosenColor);
-
 })
 
 // Randomly choose a color
 function nextSequence() {
+
+  level++;
+  $("#level-title").text("Level " + level);
+
   let randomNumber = Math.floor(Math.random() * 4);
-  let randomChosenColour = buttonColors[randomNumber];
-  gamePattern.push(randomChosenColour);
-  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
-  playsound(randomChosenColour);
+  let randomChosenColor = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColor);
+  $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColor);
+
 }
 
 function playSound(name) {
