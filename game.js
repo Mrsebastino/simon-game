a778128d52eafff08a153e3e150904142277b1f2
mrsebastino <mrsebastino@gmail.com>
2021-02-20 11:26:26 +0000

    added check answer functionality and restart game function when wrong

--- game.js
+++ game.js
@@ -19,10 +19,35 @@ $(".btn").click(function() {
    userClickedPattern.push(userChosenColor);
    playSound(userChosenColor);
    animatePress(userChosenColor);
-})
+   checkAnswer(userClickedPattern.length-1);
+});
+
+function checkAnswer(currentLevel) {
+  // checked if the most recent player answer match the game answer
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+   console.log("success");
+   // if the player does have the right answer, we triggered nextSquence
+   if (userClickedPattern.length === gamePattern.length) {
+     setTimeout(function() {
+       nextSequence();
+     }, 1000);
+   }
+ } else {
+   console.log("wrong");
+   playSound("wrong");
+   $("body").addClass("game-over");
+   setTimeout(function(){
+     $("body").removeClass("game-over");
+   }, 200);
+   $("#level-title").text("Game over, Press Any Key to Restart");
+   startOver();
+ }
+}
 
 
 function nextSequence() {
+  // when nextSequence is triggered, reset p[layer array to empty
+  userClickedPattern = [];
   // Increase level each time next Sequence is run and change text for user to see
   level++;
   $("#level-title").text("Level " + level);
@@ -49,4 +74,10 @@ function animatePress(currentColor) {
   setTimeout(function () {
     $("#" + currentColor).removeClass("pressed");
   }, 100);
+}
+
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  started = false;
 }
